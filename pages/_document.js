4b32c810653c01c683401bda9321247b95cf8473
Kanhaiya Dhaker <76777058+kanhaiya83@users.noreply.github.com>
2024-08-17 17:02:48 +0530

    add google site verification tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <meta name="google-site-verification" content="JwVXdYJqcCJlKvmJvXIMQ1XQyVZO_EyCTPXo62VOi1w" />
+      </Head>
       <body>
         <Main />
         <NextScript />
